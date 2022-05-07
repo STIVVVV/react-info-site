cafb0d4f827bf91a95b2919c8ace2eee934fd636
STIVVVV <stevencanfield2000@gmail.com>
2022-05-07 11:46:55 -0400

    Learned what imperative code is.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function MainContent()
     )
     
 }
-
+/*
 ReactDOM.render (
     <div>
         <MainContent/>
@@ -13,3 +13,8 @@ ReactDOM.render (
 
     document.getElementById("root")
 )
+*/
+const h1 = document.createElement("h1")
+h1.textContent = "carl"
+h1.className = "header"
+document.getElementById('root').appendChild(h1)
