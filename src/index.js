fc642316d8589b1a815a914850a027e44f0302af
STIVVVV <stevencanfield2000@gmail.com>
2022-05-07 22:51:49 -0400

    Learning about componets

--- src/index.js
+++ src/index.js
@@ -1,20 +1,58 @@
-function MainContent()
+import React from "react"
+import ReactDom from "react-dom"
+
+function Header()
 {
-    return (
-        <h1>I am learning react</h1>
+    return(
+        <header>
+            <nav className="nav">
+                <img src="/logo192.png" width="40px" alt=""/>
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+function MainContent() 
+{
+    return(
+        <>
+            <h1> I am happy to learn react</h1>
+            <ol>
+                <li>It is fun to learn</li>
+                <li>Making over 6 figures looks pretty nice</li>
+                <li>learning this will further improve my coding skills</li>
+            </ol>
+        </>
     )
     
 }
-/*
-ReactDOM.render (
-    <div>
-        <MainContent/>
-    </div>,
 
-    document.getElementById("root")
+function Footer() 
+{
+    return (
+        <footer>
+            <small>Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function Page() 
+{
+    return (
+        <div>
+            <Header/>
+            <MainContent/>
+           <Footer/>
+        </div>
+    )  
+}
+
+ReactDom.render
+(
+    <Page/>, document.getElementById("root")
 )
-*/
-const h1 = document.createElement("h1")
-h1.textContent = "carl"
-h1.className = "header"
-document.getElementById('root').appendChild(h1)
