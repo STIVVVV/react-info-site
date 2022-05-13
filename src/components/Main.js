2a9640fc23283c71f2eac6b0ab86b8aea9229949
STIVVVV <stevencanfield2000@gmail.com>
2022-05-13 12:24:16 -0400

    Added to the Main components

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,5 +2,16 @@ import React from "react"
 
 export default function Main()
 {
-    return <h1>Main Goes here</h1>
+    return (
+        <main>
+            <h1>Fun Fact about React</h1>
+            <ul>
+                <li>Was first Released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,9 @@ export default function Navbar()
 {
     return (
         <nav>
-            <img src={logo} alt="logo"></img>
-            <h3>React Facts</h3>
-            <h4>It's a good day</h4>
+            <img src={logo} alt="logo" className="nav--icon"></img>
+            <h3 className="nav--logo_text">React Facts</h3>
+            <h4 className="nav--title">It's a good day</h4>
         </nav>
     )
 }
