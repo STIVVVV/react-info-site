45dc26f9e6f1086d62e8fc6e4940f72e9b4d605c
STIVVVV <stevencanfield2000@gmail.com>
2022-05-13 13:30:46 -0400

    added style to the main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,8 +4,8 @@ export default function Main()
 {
     return (
         <main>
-            <h1>Fun Fact about React</h1>
-            <ul>
+            <h1 className="main--title">Fun Fact about React</h1>
+            <ul className="main--facts">
                 <li>Was first Released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100k stars on Github</li>
